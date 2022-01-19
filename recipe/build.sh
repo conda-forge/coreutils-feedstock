3f75c5d8f35e62b54d30bbade19ac40f131cd80d
ngam <67342040+ngam@users.noreply.github.com>
2022-01-19 15:10:52 -0500

    don't check when cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,11 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
 
 if [[ "$target_platform" == osx-arm64 ]]; then
     ./configure --prefix=$PREFIX --host=arm64
+    make -j $CPU_COUNT
+    make install
 else
     ./configure --prefix=$PREFIX
+    make -j $CPU_COUNT
+    make install
+    make installcheck
 fi
-
-make -j $CPU_COUNT
-make install
-make installcheck
