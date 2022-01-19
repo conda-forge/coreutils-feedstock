2baef3f687249178386260a04f3fbfe019e1e7d0
ngam <67342040+ngam@users.noreply.github.com>
2022-01-19 14:44:17 -0500

    try a better conditional

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
-./configure --prefix=$PREFIX --host=${target_platform}
+
+if [[ "$target_platform" == osx-arm64 ]]; then
+    ./configure --prefix=$PREFIX --host=arm64
+else
+    ./configure --prefix=$PREFIX
+fi
 
 make -j $CPU_COUNT
 make install
