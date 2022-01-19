27cbc37144df45915eadc2d22846c0d2df7578e1
ngam <67342040+ngam@users.noreply.github.com>
2022-01-19 14:38:55 -0500

    try adding target_platform to configure

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --host=${target_platform}
 
 make -j $CPU_COUNT
 make install
